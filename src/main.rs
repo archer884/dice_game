77ba43e7eba42137e1686a8eb44bb8059683073a
J/A <archer884@gmail.com>
2015-12-10 18:41:55 -0600

    Short version

--- src/main.rs
+++ src/main.rs
@@ -4,32 +4,20 @@ use rand::Rng;
 
 fn main() {
     loop {
-        let (player_one_score, player_two_score) = roll_for_both_players();
-        if player_one_score != player_two_score {
-            if player_one_score > player_two_score {
-                println!("Player 1 wins!");
-            } else {
-                println!("Player 2 wins!");
-            }
-            return;
+        match (&roll(), &roll()) {
+            (p1, p2) if p1 != p2 => {
+                println!("{} wins!", if p1 > p2 { "Player 1" } else { "Player 2" });
+                return;
+            },
+            _ => (),
         }
     }
 }
 
-fn roll_for_both_players() -> (i32, i32) {
-    let mut player_one_score = 0;
-    let mut player_two_score = 0;
+fn roll() -> Vec<i32> {
+    (0..3).map(|_| rng()).collect()
+}
 
-    for _ in 0..3 {
-        let roll_for_player_one = rand::thread_rng().gen_range(1, 7);
-        let roll_for_player_two = rand::thread_rng().gen_range(1, 7);
-
-        println!("Player one rolled {}", roll_for_player_one);
-        println!("Player two rolled {}", roll_for_player_two);
-
-        player_one_score += roll_for_player_one;
-        player_two_score += roll_for_player_two;
-    }
-
-    (player_one_score, player_two_score)
+fn rng() -> i32 {
+    rand::thread_rng().gen_range(1, 7)
 }
